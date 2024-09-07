6cff93d38826a33f8ceca2fc550f089d5f1f9355
lynekpng <lynekpng@gmail.com>
2024-09-07 21:35:41 +0200

    added local storage

--- my-svelte-app/src/routes/Modal.svelte
+++ my-svelte-app/src/routes/Modal.svelte
@@ -97,179 +97,204 @@
 		}
 	}
 
+
 	let selectedcategoryFunction = () => {
 		categorySelected = [{name: category.name, icon: category.icon}]
 	}
 
-	</script>
-	
-	<section>
-		<div class="modal-header">
-			<button on:click={close}>X</button>
-			<div class="title-header">
-				<h2>Add new task</h2>
-			</div>
-		</div>
-		<form on:submit={addTask}>
-			<label for="taskName">Task name :</label>
-			<br>
-			<input type="text" name="taskName" id="taskName" placeholder="Task name" bind:value={taskName} on:keyup={validate(taskName, null,  null)}>
-			{#if errors.taskName}
-			<p class="error">{errors.taskName}</p>
-			{/if}
-			<label for="category">Category :</label>
-			<br>
-			<div class="categorys">
-			{#each categorys as category}
-			
-				<div 
-				class="category {categorySelected.name === category.name ? 'selected' : ''}" 
-				role="button" 
-				tabindex="0"
-				value={category.name} 
-				on:click={() => categorySelected = category}
-				on:keydown={(event) => event.key === 'Enter' && (categorySelected = category)}
-				>
-					<img src={category.icon} alt={category.name}>
-					<p>{category.name}</p>
-				</div>
-			{/each}
-			</div>
-			<label for="taskDate">Task date :</label>
-			<br>
-			<input type="date" name="taskDate" id="taskDate" bind:value={taskDate}  on:change={validate(null, null,  taskDate)}>
-			{#if errors.taskDate}
-			<p class="error">{errors.taskDate}</p>
-			{/if} 
-	
-			<label for="taskDescription">Task description :</label>
-			<textarea name="description" id="taskDescription" cols="30" rows="10" bind:value={taskDescription} on:keyup={validate(null, taskDescription, null )}></textarea>
-			{#if errors.taskDescription}
-			<p class="error">{errors.taskDescription}</p>
-			{/if}
-	
-			
-	
-			<input type="submit" value="Ajouter la tâche">
-		</form>
-	
-		<h1>{taskName}</h1>
-	</section>
-	
-	<style scoped lang="scss">
-		section {
-			width: 100%;
-			display: flex;
-			flex-direction: column;
-			justify-content: center;
-			align-items: center;
-			flex: 0.6;
+	function saveTask(name, date, description, category) {
+    const task = { name, date, description, category };
+    localStorage.setItem('task', JSON.stringify(task)); 
+    
+  }
+</script>
 
-			.modal-header {
-				width: 100%;
-				background-image: url('./../lib/images/Header\ \(1\).svg');
-				background-size: cover;
-				background-color: #4A3780;
-				color: white;
-				padding: 10px;
-				display: flex;
-				flex-direction: row;
-				justify-content: space-between;
+<section>
+  <div class="modal-header">
+    <button on:click={close}>X</button>
+    <div class="title-header">
+      <h2>Add new task</h2>
+    </div>
+  </div>
+  <form on:submit={addTask}>
+    <label for="taskName">Task name :</label>
+    <br />
+    <input
+      type="text"
+      name="taskName"
+      id="taskName"
+      placeholder="Task name"
+      bind:value={taskName}
+      on:keyup={validate(taskName, null, null)}
+    />
+    {#if errors.taskName}
+      <p class="error">{errors.taskName}</p>
+    {/if}
+    <label for="category">Category :</label>
+    <br />
+    <div class="categorys">
+      {#each categorys as category}
+        <div
+          class="category {categorySelected.name === category.name
+            ? 'selected'
+            : ''}"
+          role="button"
+          tabindex="0"
+          value={category.name}
+          on:click={() => (categorySelected = category)}
+          on:keydown={(event) =>
+            event.key === "Enter" && (categorySelected = category)}
+        >
+          <img src={category.icon} alt={category.name} />
+          <p>{category.name}</p>
+        </div>
+      {/each}
+    </div>
+    <label for="taskDate">Task date :</label>
+    <br />
+    <input
+      type="date"
+      name="taskDate"
+      id="taskDate"
+      bind:value={taskDate}
+      on:change={validate(null, null, taskDate)}
+    />
+    {#if errors.taskDate}
+      <p class="error">{errors.taskDate}</p>
+    {/if}
 
-				button {
-					border-radius: 100%;
-					border: none;
-					width: 50px;
-					height: 50px;
-					cursor: pointer;
-				}
+    <label for="taskDescription">Task description :</label>
+    <textarea
+      name="description"
+      id="taskDescription"
+      cols="30"
+      rows="10"
+      bind:value={taskDescription}
+      on:keyup={validate(null, taskDescription, null)}
+    ></textarea>
+    {#if errors.taskDescription}
+      <p class="error">{errors.taskDescription}</p>
+    {/if}
 
-				.title-header {
-					width: 55%;
-					text-align: left;
-				}
-			}
-	
-			form {
-				position: relative;
-				width: 100%;
-				height: 95vh;
-				display: flex;
-				flex-direction: column;
-				justify-content: space-around;
-				align-items: center;
-				gap: 5px;
-				padding-top: 10px;
+    <input type="submit" on:click={() => saveTask(taskName, taskDate, taskDescription, categorySelected)} value="Ajouter la tâche" />
+  </form>
 
-				label {
-					font-weight: bold;
-					margin-top: 10px;
-				}
-				input {
-					width: 90%;
-					min-height: 50px;
-					padding-left: 5px;
-					border-radius: 5px;
-				}
-				textarea {
-					margin-top: 15px;
-					border-radius: 5px;
-				}
-				input[type=submit] {
-					margin-top: 30px;
-					margin-bottom: 30px;
-					background-color: #4A3780;
-					color: white;
-					border-radius: 60px;
-				}
+  <h1>{taskName}</h1>
+</section>
 
-				.categorys {
-					display: flex;
-					flex-direction: row;
-					gap: 25px;
-					.category {
-						padding: 10px;
-						width: 70px;
-						height: 100px !important;
-						border-radius: 15px;
-						display: flex;
-						flex-direction: column;
-						justify-content: space-between;
-						align-items: center;
-						&:hover {
-							background-color: rgba(0, 255, 255, 0.222);
-						}
-						img {
-							width: 80%;
-						}
-						p {
-							font-weight: bold;
-						}
-					}
-					.selected {
-						background-color: rgba(0, 255, 187, 0.222);
-						border: 2px black solid;
-					}
-				}
+<style scoped lang="scss">
+  section {
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    flex: 0.6;
 
-				textarea {
-					width: 90%;
-					min-height: 150px;
-				}
-				
-				.error {
-					background-color: tomato;
-					padding: 5px 15px;
-					border-radius: 5px;
-					color: white;
-					font-size: 0.9em;
-					font-weight: bold;
-				}
-			}
-		}
-	
-		h1 {
-			width: 100%;
-		}
-	</style>
-	
+    .modal-header {
+      width: 100%;
+      background-image: url("./../lib/images/Header\ \(1\).svg");
+      background-size: cover;
+      background-color: #4a3780;
+      color: white;
+      padding: 10px;
+      display: flex;
+      flex-direction: row;
+      justify-content: space-between;
+
+      button {
+        border-radius: 100%;
+        border: none;
+        width: 50px;
+        height: 50px;
+        cursor: pointer;
+      }
+
+      .title-header {
+        width: 55%;
+        text-align: left;
+      }
+    }
+
+    form {
+      position: relative;
+      width: 100%;
+      height: 95vh;
+      display: flex;
+      flex-direction: column;
+      justify-content: space-around;
+      align-items: center;
+      gap: 5px;
+      padding-top: 10px;
+
+      label {
+        font-weight: bold;
+        margin-top: 10px;
+      }
+      input {
+        width: 90%;
+        min-height: 50px;
+        padding-left: 5px;
+        border-radius: 5px;
+      }
+      textarea {
+        margin-top: 15px;
+        border-radius: 5px;
+      }
+      input[type="submit"] {
+        margin-top: 30px;
+        margin-bottom: 30px;
+        background-color: #4a3780;
+        color: white;
+        border-radius: 60px;
+      }
+
+      .categorys {
+        display: flex;
+        flex-direction: row;
+        gap: 25px;
+        .category {
+          padding: 10px;
+          width: 70px;
+          height: 100px !important;
+          border-radius: 15px;
+          display: flex;
+          flex-direction: column;
+          justify-content: space-between;
+          align-items: center;
+          &:hover {
+            background-color: rgba(0, 255, 255, 0.222);
+          }
+          img {
+            width: 80%;
+          }
+          p {
+            font-weight: bold;
+          }
+        }
+        .selected {
+          background-color: rgba(0, 255, 187, 0.222);
+          border: 2px black solid;
+        }
+      }
+
+      textarea {
+        width: 90%;
+        min-height: 150px;
+      }
+
+      .error {
+        background-color: tomato;
+        padding: 5px 15px;
+        border-radius: 5px;
+        color: white;
+        font-size: 0.9em;
+        font-weight: bold;
+      }
+    }
+  }
+
+  h1 {
+    width: 100%;
+  }
+</style>
